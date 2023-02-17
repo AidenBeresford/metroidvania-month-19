0bd9de690d27243397229382b3d7296713074687
AidenBeresford <aidenberes@gmail.com>
2023-02-16 23:21:57 -0600

    Basic state system

--- scripts/PlayerPhysics.gd
+++ scripts/PlayerPhysics.gd
@@ -1,17 +1,25 @@
 extends KinematicBody2D
 
+enum STATE {IDLE, WALK, DASH, JUMP, FALL, DEATH}
+enum DIR {LEFT, RIGHT, DOWN, UP}
+
 var velocity = Vector2()
+var state = STATE
 
 const SPD = 180
+const JMP = -300
 
 
 func _physics_process(delta):
-	move_player()
+	walk()
+	jump()
+	
+	velocity.y -= 10
 	
 	move_and_slide(velocity)
 
 
-func move_player():
+func walk():
 	
 	if Input.is_action_pressed("right"):
 		velocity.x = lerp(velocity.x, SPD, .20)
@@ -19,3 +27,10 @@ func move_player():
 		velocity.x = lerp(velocity.x, -SPD, .20)
 	else:
 		velocity.x = lerp(velocity.x, 0, .20)
+
+
+func jump():
+	
+	if Input.is_action_pressed("jump"):
+		velocity.y = JMP
+	
