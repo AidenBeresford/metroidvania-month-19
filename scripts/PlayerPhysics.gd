5a8d3e2428a5b166a696b41522e7d32db1d5e711
AidenBeresford <aidenberes@gmail.com>
2023-02-17 15:47:44 -0600

    Added state match

--- scripts/PlayerPhysics.gd
+++ scripts/PlayerPhysics.gd
@@ -1,26 +1,46 @@
 extends KinematicBody2D
 
 enum STATE {IDLE, WALK, DASH, JUMP, FALL, DEATH}
-enum DIR {LEFT, RIGHT, DOWN, UP}
-
+enum DIR {LEFT, RIGHT}
 var velocity = Vector2()
 var state = STATE
+var dir = DIR.RIGHT
 
 const SPD = 180
+const DSH = 1.5
 const JMP = -500
+const GRV = -25
 
 
 func _physics_process(delta):
 	
-	# state manager should always run first
+	velocity.y += 25
+	
 	state_manager()
 	
-	# these will be added to a match statement later
-	walk()
-	jump()
-	
-	# we always want this to run
-	velocity.y += 25
+	# player abilities based on state
+	match state:
+		
+		STATE.IDLE:
+			walk()
+			jump()
+		
+		STATE.WALK:
+			walk()
+			jump()
+		
+		STATE.DASH:
+			jump()
+			dash()
+		
+		STATE.JUMP:
+			walk()
+		
+		STATE.FALL:
+			walk()
+		
+		STATE.DEATH:
+			pass
 	
 	move_and_slide(velocity, Vector2.UP)
 
@@ -29,6 +49,8 @@ func state_manager():
 	
 	if is_on_floor():
 		
+		velocity.y = 0
+		
 		if Input.is_action_pressed("dash"):
 			state = STATE.DASH
 		else:
@@ -36,11 +58,16 @@ func state_manager():
 				state = STATE.WALK
 			else:
 				state = STATE.IDLE
-	else:
+	elif !Input.is_action_pressed("dash"):
 		if velocity.y >= 0:
 			state = STATE.JUMP
 		else:
 			state = STATE.FALL
+	
+	if Input.is_action_pressed("right"):
+		dir = DIR.RIGHT
+	elif Input.is_action_pressed("left"):
+		dir = DIR.LEFT
 
 
 func walk():
@@ -59,4 +86,14 @@ func jump():
 	# jump command for idle and walk states
 	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JMP
+
+func dash():
 	
+	if Input.is_action_pressed("dash"):
+		if dir == DIR.LEFT:
+			velocity.x = SPD*-DSH
+		elif dir == DIR.RIGHT:
+			velocity.x = SPD*DSH
+	
+	if Input.is_action_just_released("dash"):
+		velocity.x = 0
