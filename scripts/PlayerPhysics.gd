b6447636047a664d2a2d6d55782724844ea2f9ac
AidenBeresford <aidenberes@gmail.com>
2023-02-16 23:38:46 -0600

    Fixed jump, created debug room

--- scripts/PlayerPhysics.gd
+++ scripts/PlayerPhysics.gd
@@ -7,20 +7,45 @@ var velocity = Vector2()
 var state = STATE
 
 const SPD = 180
-const JMP = -300
+const JMP = -500
 
 
 func _physics_process(delta):
+	
+	# state manager should always run first
+	state_manager()
+	
+	# these will be added to a match statement later
 	walk()
 	jump()
 	
-	velocity.y -= 10
+	# we always want this to run
+	velocity.y += 25
 	
-	move_and_slide(velocity)
+	move_and_slide(velocity, Vector2.UP)
+
+
+func state_manager():
+	
+	if is_on_floor():
+		
+		if Input.is_action_pressed("dash"):
+			state = STATE.DASH
+		else:
+			if velocity.x != 0:
+				state = STATE.WALK
+			else:
+				state = STATE.IDLE
+	else:
+		if velocity.y >= 0:
+			state = STATE.JUMP
+		else:
+			state = STATE.FALL
 
 
 func walk():
 	
+	# walk command for idle and walk states
 	if Input.is_action_pressed("right"):
 		velocity.x = lerp(velocity.x, SPD, .20)
 	elif Input.is_action_pressed("left"):
@@ -31,6 +56,7 @@ func walk():
 
 func jump():
 	
-	if Input.is_action_pressed("jump"):
+	# jump command for idle and walk states
+	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JMP
 	
